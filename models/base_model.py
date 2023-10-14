ef7c039431a01d91c98cc06b4a3abe7662a92ee6
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-14 20:02:16 +0300

    file check

---
+++ models/base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+defines all common attributes/methods for other classes
+"""
+
+
+import models
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """class Base for the all the model"""
+
+    def __init__(self, *args, **kwargs):
+        """class constructor init"""
+
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """string of BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """updates 'updated_at' instance with current datetime"""
+
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """dictionary representation of instance"""
+
+        updated_dic = dict(self.__dict__)
+        updated_dic['created_at'] = self.__dict__['created_at'].isoformat()
+        updated_dic['updated_at'] = self.__dict__['updated_at'].isoformat()
+        updated_dic['__class__'] = self.__class__.__name__
+        return (updated_dic)
