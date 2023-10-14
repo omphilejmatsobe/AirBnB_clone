8a6eeef93bed63d37b6702f4d3f920eeefce1fe6
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-15 02:10:58 +0300

    file check

---
+++ models/engine/file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""file_storage.py module"""
+import json
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.amenity import Amenity
+from models.state import State
+
+
+class FileStorage():
+    """
+    FileStorage class:
+    ------------------
+    """
