3e6ab2d71e9292881a224adfa88affae0b4a0bf1
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-15 23:33:25 +0300

    file check

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class state that inherites from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    class state
+    """
+    name = ""
