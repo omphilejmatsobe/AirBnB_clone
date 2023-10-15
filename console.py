1f0fcbd1b61bc7425f6e6b34cc48c0b97ea97896
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-16 01:25:43 +0300

    file test

--- console.py
+++ console.py
@@ -267,14 +267,14 @@ class HBNBCommand(cmd.Cmd):
         elif args[1] == "update":
             params = re.match(r"\"(.+?)\", (.+)", args[2])
             if params.groups()[1][0] == '{':
-                dic_p = eval(params.groups()[1])
-                for k, v in dic_p.items():
+                _dict = eval(params.groups()[1])
+                for k, v in _dict.items():
                     commands[args[1]](args[0] + " " + params.groups()[0] +
                                       " " + k + " " + str(v))
             else:
-                rest = params.groups()[1].split(", ")
+                i = params.groups()[1].split(", ")
                 commands[args[1]](args[0] + " " + params.groups()[0] + " " +
-                                  rest[0] + " " + rest[1])
+                                  i[0] + " " + i[1])
 
 
 if __name__ == '__main__':
