683a0582383af36cea4971b1375e6ebab37dc59e
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-15 23:08:03 +0300

    file check

--- console.py
+++ console.py
@@ -11,7 +11,11 @@ from models import storage
 
 from models.base_model import BaseModel
 from models.user import User
-
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 all_classes = {'BaseModel': BaseModel, 'User': User,
                'State': State, 'City': City, 'Amenity': Amenity,
